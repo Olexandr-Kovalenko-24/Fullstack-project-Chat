7d91f7e69198c52a9ab33b6d99b2a60c01631b8f
Olexandr Kovalenko <22bendera.s@gmail.com>
2023-01-31 18:04:58 +0200

    Function generator

---
+++ sandbox.js
@@ -0,0 +1,44 @@
+// function* generate() {
+//     console.log(1);
+//     yield 42;
+//     console.log(2);
+//     const newArgument = yield 6;
+//     console.log(newArgument);
+//     return 13
+// }
+
+// const res = generate();
+// console.log(res);
+
+// console.log(res.next());
+
+// console.log(res.next());
+
+// console.log(res.next('Super-value'));
+
+
+function* incrementToHundred(){
+    let sum = 0;
+    for (let i = 0; i < 100; i++) {
+        sum+=i;
+        console.log(sum);
+        yield i
+    }
+    return 100
+}
+
+function* generateNumber(){
+    let score = 0;
+    let sum = 0;
+    while (score < 100) {
+        sum+=score;
+        console.log(sum);
+        yield score++;
+    }
+}
+
+const result = generateNumber();
+console.log(result.next());
+console.log(result.next());
+console.log(result.next());
+console.log(result.next());
